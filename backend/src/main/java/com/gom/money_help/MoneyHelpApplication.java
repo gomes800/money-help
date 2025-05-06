228d0c79d41add397e069906a25375581991e50e
gomes <joaogomesleite800@gmail.com>
2025-05-06 13:00:43 -0300

    first commit

---
+++ backend/src/main/java/com/gom/money_help/MoneyHelpApplication.java
@@ -0,0 +1,13 @@
+package com.gom.money_help;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyHelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyHelpApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gom/money_help/MoneyHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gom.money_help;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyHelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
