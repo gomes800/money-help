4fd54e041bb0eeef8bfe75207657b7c4a0f6d630
gomes <joaogomesleite800@gmail.com>
2025-05-16 10:53:25 -0300

    feat: create page add expense

--- money-frontend/src/app/app.routes.ts
+++ money-frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ExpensesComponent } from './pages/expenses/expenses.component';
+import { AddExpenseComponent } from './pages/add-expense/add-expense.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'expenses', component: ExpensesComponent},
+    { path: 'addExpense', component: AddExpenseComponent},
 ];
