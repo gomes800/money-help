2e8d2f6d2c6c0215e20f9be35b80f38208a120ab
gomes <joaogomesleite800@gmail.com>
2025-05-23 10:29:07 -0300

    feat: create user role enum and implement user details

--- backend/src/main/java/com/gom/money_help/model/User.java
+++ backend/src/main/java/com/gom/money_help/model/User.java
@@ -3,16 +3,20 @@ package com.gom.money_help.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Data
 @Table(name = "users")
-public class User implements Serializable {
+public class User implements Serializable, UserDetails {
 
     @Serial
     private static final long serialVersionUID = 1L;
@@ -23,8 +27,20 @@ public class User implements Serializable {
     private String name;
     private String login;
     private String password;
+    private UserRole role;
     private Long balanceInCents = 0L;
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonIgnore
     private List<Expense> expenses = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
 }
--- backend/src/main/java/com/gom/money_help/repositories/UserRepository.java
+++ backend/src/main/java/com/gom/money_help/repositories/UserRepository.java
@@ -2,6 +2,9 @@ package com.gom.money_help.repositories;
 
 import com.gom.money_help.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    UserDetails findByLogin(String login);
 }
