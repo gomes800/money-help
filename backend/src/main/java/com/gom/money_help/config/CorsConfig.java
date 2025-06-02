c075c1e489af95b84559aa4b487d81cf26dbcf91
gomes <joaogomesleite800@gmail.com>
2025-06-02 15:27:59 -0300

    delete frontend

--- backend/src/main/java/com/gom/money_help/config/CorsConfig.java
+++ backend/src/main/java/com/gom/money_help/config/CorsConfig.java
@@ -10,7 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(
+                        "http://localhost:4200",
+                        "https://gomes800.github.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
